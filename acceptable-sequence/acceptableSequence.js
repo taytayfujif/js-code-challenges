416328a89ea31d9f6c82f2b77e5e7957dafc377d
Taylor <tyafujimoto@gmail.com>
2018-08-22 14:39:50 -1000

    first commit

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,8 @@
 module.exports = function acceptableSequence(str) {
+ console.log('str', str);
 
 }
+module.exports = function simplesymbols(str) {
+    var abc= 'abcdefghijklmnopqrstuvwxyz';
+var checkstring= abc.indexOf('p');
+console.log(checkstring)
